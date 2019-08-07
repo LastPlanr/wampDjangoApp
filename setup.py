13ef5554420e99147b67f25631348150dc1dd62d
Cléber Zavadniak <cleber@dronemapp.com>
2019-08-06 21:28:33 -0300

    bugfix on setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = '0.1.1'
+version = '0.1.2'
 
-with open('requirements/production.txt') as requirements_file:
-    requires = [item for item in requirements_file]
+requires = []
 
 with open('LICENSE') as f:
     license = f.read()
